e8d4346109e5a393437228d7b58ebef6e9b1472e
AcheDiamond <acchavez@student.apc.edu.ph>
2026-01-17 07:01:44 +0000

    boorgir

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Boorgir</h1>
+  <food-item/>
+  <food-item2/>
+</template>
+
+<script></script>
+
+<style></style>
---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="mango">
+    <h2>{{ name }}</h2>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'Boorgir/ Sandwitch',
+      message: 'I like Habit, In-N-Out, Chic-fil-A'
+    }
+  }
+};
+</script>
+
+<style>
+  .Boorgir {
+    border: dashed rgba(244, 178, 178, 0.84) 1px;
+    display: inline-block;
+    margin: 10px;
+    padding: 10px;
+    background-color: rgb(203, 0, 0);
+  }
+</style>
