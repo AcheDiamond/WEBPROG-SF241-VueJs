b2903e336573a79c454787b68e9d5be6f78fc143
AcheDiamond <acchavez@student.apc.edu.ph>
2026-01-27 12:38:34 +0800

    created the supabase client

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,7 +1,21 @@
-<template>
-  <PersonalProfile />
-</template>
-
 <script setup>
-import PersonalProfile from './components/PersonalProfile.vue'
+import { ref, onMounted } from 'vue'
+import { supabase } from './lib/supabaseClient'
+
+const instruments = ref([])
+
+async function getInstruments() {
+  const { data } = await supabase.from('instruments').select()
+  instruments.value = data
+}
+
+onMounted(() => {
+   getInstruments()
+})
 </script>
+
+<template>
+  <ul>
+    <li v-for="instrument in instruments" :key="instrument.id">{{ instrument.name }}</li>
+  </ul>
+</template>
